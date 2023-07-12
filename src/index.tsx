92a073b56c930f43e64e68c5a0a4bfa656677baa
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-07-12 09:28:46 -0400

    (╯‵□′)╯︵┻━┻

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import "./index.css";
 
 import { StrictMode, Suspense } from "react";
 
+import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import { Loading } from "./Loading.tsx";
-import { createRoot } from "react-dom/client";
 
 // Mock Service Worker
 if (process.env.NODE_ENV === "development") {
     // MOCK value comes from the webpack.DefinePlugin
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
+    // Test
     if (USE_MSW) {
         import("./mocks/browser.ts").then(({ worker }) => {
             worker.start();
